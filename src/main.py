88c94c0e4faf44c0f973f1c3f6d8cc31692b5174
ico0018 <siliwang.ico@gmail.com>
2023-05-27 05:17:28 +0800

    Add bilingual para

--- src/main.py
+++ src/main.py
@@ -2,27 +2,29 @@ import os
 import openai
 from tqdm import tqdm
 import time
+import argparse
+
+# 创建解析步骤
+parser = argparse.ArgumentParser(description='Translate text files.')
+parser.add_argument('--bilingual', action='store_true', help='Print both the original and translated text.')
+
+# 解析参数
+args = parser.parse_args()
 
 openai.api_key = os.getenv("OPENAI_KEY")
 
-# 定义输入输出目录
 input_directory = '../Original'
 output_directory = '../English'
 
-# 定义句子结束的标志
 end_of_sentence = ['。', '！', '？']
 
-# 定义搜索句子结束标志的范围
-N = 50  # 你可以根据实际情况调整这个值
+N = 50  
 
-# 遍历文件夹
 for filename in os.listdir(input_directory):
     if filename.endswith(".txt"):
-        # 读取文件
         with open(os.path.join(input_directory, filename), 'r', encoding='utf-8') as f:
             text = f.read()
 
-        # 将文本分割成多个部分
         parts = []
         while text:
             if len(text) > 1024:
@@ -45,27 +47,25 @@ for filename in os.listdir(input_directory):
                 parts.append(text)
                 text = ''
 
-        # 打开输出文件
         with open(os.path.join(output_directory, filename), 'w', encoding='utf-8') as f:
             for part in tqdm(parts, desc=f"Translating {filename}", unit="part"):
                 while True:
                     try:
-                        # 使用OpenAI API翻译文本
                         response = openai.ChatCompletion.create(
-                            model="gpt-3.5-turbo",  # 这里需要使用你想要的模型，如GPT-4
+                            model="gpt-3.5-turbo",
                             messages=[
                                 {"role": "system", "content": "You are a translator."},
                                 {"role": "user", "content": f"Translate this into English:\n\n{part}"}
                             ]
                         )
-                        # 将请求的信息和翻译结果写入到文本文件中
-                        f.write(f"Request:\n{part}\n")
-                        f.write(f"Response:\n{response['choices'][0]['message']['content']}\n")
-                        time.sleep(0.1)  # 在每个请求之间添加0.1秒的延迟
+                        if args.bilingual:
+                            f.write(f"{part}\n")
+                        f.write(f"{response['choices'][0]['message']['content']}\n")
+                        time.sleep(0.1)
                         break
                     except openai.error.RateLimitError:
                         print("Rate limit exceeded, sleeping for a while...")
-                        time.sleep(60)  # 等待60秒
+                        time.sleep(60)
                     except openai.error.OpenAIError as e:
                         print(f"An error occurred: {e}")
                         break
